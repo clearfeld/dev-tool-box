0358c63fc62249f62c6056a22e4d0b0b11ab0bb5
Kevin Ly <44701340+kevinly77@users.noreply.github.com>
2024-01-09 03:17:18 -0500

    Add: string conversions with dropdown

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -6,53 +6,55 @@ import Sidebar from './components/sidebar';
 import StringCaseConverter from './tools/strings/string-case-converter';
 
 function CardLink(props) {
-  return (
-    <Link to="/tool/string/stringcase">
-      <div
-        style={{
-          backgroundColor: 'var(--color-global-dark-gray-hover)',
-          margin: '1rem',
-          padding: '0.5rem',
-          borderRadius: '0.5rem',
-        }}
-      >
-        <h2>{props.title}</h2>
-      </div>
-    </Link>
-  );
+	return (
+		<Link to="/tool/string/stringcase">
+			<div
+				style={{
+					backgroundColor: 'var(--color-global-dark-gray-hover)',
+					margin: '1rem',
+					padding: '0.5rem',
+					borderRadius: '0.5rem',
+				}}
+			>
+				<h2>{props.title}</h2>
+			</div>
+		</Link>
+	);
 }
 
 function Hello() {
-  return (
-    <div>
-      <Navbar />
+	return (
+		<div>
+			<Navbar />
+			<Sidebar />
 
-      <Sidebar />
-
-      <div
-        style={{
-          position: 'absolute',
-          marginLeft: 'var(--sidebar-size)',
-          marginTop: '4rem',
-          top: '0',
-          left: '0',
-        }}
-      >
-        <CardLink title="String Case Converter" />
-      </div>
-    </div>
-  );
+			<div
+				style={{
+					position: 'absolute',
+					marginLeft: 'var(--sidebar-size)',
+					marginTop: '4rem',
+					top: '0',
+					left: '0',
+				}}
+			>
+				<CardLink title="String Case Converter" />
+			</div>
+		</div>
+	);
 }
 
 export default function App() {
-  return (
-    <div>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Hello />} />
-          <Route path="/tool/string/stringcase" element={<StringCaseConverter />} />
-        </Routes>
-      </Router>
-    </div>
-  );
+	return (
+		<div>
+			<Router>
+				<Routes>
+					<Route path="/" element={<Hello />} />
+					<Route
+						path="/tool/string/stringcase"
+						element={<StringCaseConverter />}
+					/>
+				</Routes>
+			</Router>
+		</div>
+	);
 }
--- src/renderer/tools/strings/string-case-converter/index.tsx
+++ src/renderer/tools/strings/string-case-converter/index.tsx
@@ -1,4 +1,4 @@
-import { ChangeEvent, FormEvent, useState } from 'react';
+import { ChangeEvent, FormEvent, useEffect, useState } from 'react';
 import Navbar from '../../../components/navbar';
 import Sidebar from '../../../components/sidebar';
 import './string-case-converter.css';
@@ -8,41 +8,87 @@ import { Button } from '@controlkit/button';
 // convert everything into rem/em
 // ask about making things full width/height without breaking shit (position absolute)
 // ENUM for options? make it a type?
-// think about how I want to handle dropdown text changes
+
+//try rearranging things using grid for layout
 
 function StringCaseConverter() {
-  const [inputValue, setInputValue] = useState('');
-  const [outputValue, setOutputValue] = useState(''); 
-  const [selectedOption, setSelectedOption] = useState('UPPER CASE');
+	const [inputValue, setInputValue] = useState('');
+	const [outputValue, setOutputValue] = useState('');
+	const [selectedOption, setSelectedOption] = useState('UPPER CASE');
 
-  const options = [
-    "UPPER CASE",
-    "lower case",
-    "camelCase",
-    "snake_case",
-    "kebab-case"
-  ]
+	const options = [
+		'UPPER CASE',
+		'lower case',
+		'camelCase',
+		'snake_case',
+		'kebab-case',
+	];
 
-  function HandleInputChange(event: ChangeEvent<HTMLTextAreaElement>) {
-    setInputValue(event.target.value);
-    console.log(selectedOption);
-    switch(selectedOption) {
-      case "UPPER CASE":
-        return ConvertToUpperCase(event.target.value);
-      case "lower case":
-        return ConvertToLowerCase(event.target.value);
-    }
+	useEffect(() => {
+		switch (selectedOption) {
+			case 'UPPER CASE':
+				return ConvertToUpperCase(inputValue);
+			case 'lower case':
+				return ConvertToLowerCase(inputValue);
+			case 'camelCase':
+				return ConvertToCamelCase(inputValue);
+			case 'snake_case':
+				return ConvertToSnakeCase(inputValue);
+			case 'kebab-case':
+				return ConvertToKebabCase(inputValue);
+		}
+	}, [selectedOption, inputValue]);
 
-  }
+	function HandleInputChange(event: ChangeEvent<HTMLTextAreaElement>) {
+		setInputValue(event.target.value);
+	}
 
+	function ConvertToKebabCase(input: string) {
+		let words = input
+			.split(' ')
+			.map((word) => {
+				return word.toLocaleLowerCase();
+			})
+			.join('-');
 
-  function ConvertToUpperCase(input: string) {
-    setOutputValue(input.toLocaleUpperCase());
-  }
+		setOutputValue(words);
+	}
 
-  function ConvertToLowerCase(input: string) {
-    setOutputValue(input.toLowerCase());
-  }
+	function ConvertToSnakeCase(input: string) {
+		let words = input
+			.split(' ')
+			.map((word) => {
+				return word.toLocaleLowerCase();
+			})
+			.join('_');
+
+		setOutputValue(words);
+	}
+
+	function ConvertToCamelCase(input: string) {
+		let words = input
+			.split(' ')
+			.map((word, index) => {
+				if (index == 0) {
+					return word.toLowerCase();
+				}
+
+				return (
+					word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()
+				);
+			})
+			.join('');
+
+		setOutputValue(words);
+	}
+
+	function ConvertToUpperCase(input: string) {
+		setOutputValue(input.toLocaleUpperCase());
+	}
+
+	function ConvertToLowerCase(input: string) {
+		setOutputValue(input.toLowerCase());
+	}
 	return (
 		<div>
 			<Navbar />
@@ -55,24 +101,38 @@ function StringCaseConverter() {
 						<button className="string-case-converter__button">
 							Clear
 						</button>
-            <select onChange={(event) => setSelectedOption(event.target.value)}>
-              {options.map((option) => {
-                return (<option key={option} value={option}>{option}</option>)     
-              })}
-            </select>
+						<select
+							onChange={(event) =>
+								setSelectedOption(event.target.value)
+							}
+						>
+							{options.map((option) => {
+								return (
+									<option key={option} value={option}>
+										{option}
+									</option>
+								);
+							})}
+						</select>
 					</div>
-
-					<textarea className='string-case-converter__text-input' value={inputValue} onChange={HandleInputChange}></textarea>
+					<textarea
+						className="string-case-converter__text-area"
+						value={inputValue}
+						onChange={HandleInputChange}
+					></textarea>
 				</div>
 				<div className="string-case-converter__right">
 					<div className="string-case-converter__wrapper">
 						<h1>Output</h1>
 						<button className="string-case-converter__button">
-							Clear
+							Copy
 						</button>
 					</div>
 
-					<textarea className='string-case-converter__text-output' value={outputValue}></textarea>
+					<textarea
+						className="string-case-converter__text-area"
+						value={outputValue}
+					></textarea>
 				</div>
 			</div>
 		</div>
