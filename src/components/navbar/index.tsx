24bfdf3368f6f5a5b5c49fb82c0a899154367b42
Marko <49600278+clearfeld@users.noreply.github.com>
2024-01-16 14:53:10 -0500

    Add: icons to navbar and style

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,43 +1,65 @@
 // import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
-import { Link } from 'react-router-dom';
-import icon from '../../assets/icon.svg';
-import './Navbar.css';
+import { Link } from "react-router-dom";
+import icon from "../../assets/icon.svg";
+import "./Navbar.scss";
+
+import toolsIcon from "../../assets/svgs/tools.svg";
+import snippetIcon from "../../assets/svgs/code.svg";
+import sheetsIcon from "../../assets/svgs/papers.svg";
 
 function Navbar() {
 	return (
-		<div className="navbar">
+		<div className="navbar__wrapper">
 			<Link to="/">
 				<img width="32" alt="icon" src={icon} />
 			</Link>
 
-			<div>
-				<Link to="/snippets">Snippets</Link>
+			<div className="navbar__right-side">
+				<Link to="/">
+					<div className="navbar__link-wrap">
+						<img
+							src={toolsIcon}
+							className="svg-filter"
+							style={{
+								width: "1rem",
+								height: "1rem",
+							}}
+						/>
+
+						<p className="navbar__link-text">Tools</p>
+					</div>
+				</Link>
+
+				<Link to="/snippets">
+					<div className="navbar__link-wrap">
+						<img
+							src={snippetIcon}
+							className="svg-filter"
+							style={{
+								width: "1rem",
+								height: "1rem",
+							}}
+						/>
+
+						<p className="navbar__link-text">Snippets</p>
+					</div>
+				</Link>
+
+				<Link to="/cheatsheets">
+					<div className="navbar__link-wrap">
+						<img
+							src={sheetsIcon}
+							className="svg-filter"
+							style={{
+								width: "1rem",
+								height: "1rem",
+							}}
+						/>
+
+						<p className="navbar__link-text">Cheat Sheets</p>
+					</div>
+				</Link>
 			</div>
-
-			<div>
-				<Link to="/cheatsheets">Cheat Sheets</Link>
-			</div>
-
-			{/* <div>
-        <input type="text" />
-      </div>
-
-
-
-      <div>
-        <a
-          href="https://electron-react-boilerplate.js.org/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <button type="button">
-            <span role="img" aria-label="books">
-              📚
-            </span>
-            Read our docs
-          </button>
-        </a>
-      </div> */}
 		</div>
 	);
 }
